05cbb0255366bbcd72683a4a99314060d9162ce8
dina fadila <39585270+dinafadila@users.noreply.github.com>
2020-04-28 09:48:54 +0700

    Update ProjectDaming.r

--- ProjectDaming.r
+++ ProjectDaming.r
@@ -1,5 +1,6 @@
 data = read.csv('~/googleplaystore.csv',header=TRUE, sep=',')
 
+#---------------- 1. Pra Proses Data--------------------------
 # Eksplorasi Data
 summary(data)
 str(data)
@@ -8,7 +9,29 @@ str(data)
 library(mice)
 md.pattern(data) 
 
-# Mengisi Missing Value
-data$Rating[is.na(data$Rating)] <- names(sort(-table(data$Rating)))[1]
+# Menghilangkan missing value
+data<-data[complete.cases(data),] 
 md.pattern(data)
+
+# Menghilangkan string + dan , serta mengubah ke numeric
+data$Installs<-str_replace_all(data$Installs,"[+]","")
+data$Installs<-str_replace_all(data$Installs,",","")
+
+
+# Menghilangkan string M dan k 
+data$Size<-str_replace_all(data$Size,"M","")
+data$Size<-str_replace_all(data$Size,"k","")
+
+# Menghilangkan string $
+data$Price <- gsub('[$]', '', data$Price)
+
+# Ubah factor ke numeric
+data$Installs<-as.numeric(as.character(data$Installs))
+data$Size<-as.numeric(as.character(data$Size))
+data$Price <- as.numeric(as.character(data$Price))
+data$Reviews <- as.numeric(as.character(data$Reviews))
+
+data<-data[complete.cases(data),] 
+
+str(data)
 
